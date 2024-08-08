c6956e4b74c71dcdaed1def8c6b676717ecd774a
Ren Shyuen <srs55@student.london.ac.uk>
2024-08-08 14:47:10 +0800

    added Create Room page

--- khanfused/src/MainPage.js
+++ khanfused/src/MainPage.js
@@ -1,14 +1,23 @@
 import logo from "./Assets/Khanfused.svg";
 import './MainPage.css';
+import { useNavigate } from "react-router-dom";
 
 function MainPage() {
+
+    const navigate = useNavigate();
+
+    // ren shyuen: redirect to create room page
+    const handleCreateRoomClick = () => {
+        navigate('/create-room');
+    }
+
     return (
         <div className="global">
             <div className="logo">
                 <img src={logo} alt="Khanfused Logo" />
             </div>
             <div className="button-container">
-                <button className="button">Create Room</button>
+                <button className="button" onClick={handleCreateRoomClick}>Create Room</button>
                 <button className="button">Join Room</button>
             </div>
         </div>
