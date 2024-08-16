012acaa67aa7019ce733583fa96dda7e447c3911
Ren Shyuen <srs55@student.london.ac.uk>
2024-08-16 17:14:10 +0800

    Modify CSS Layout for players lobby page

--- khanfused/src/RoomPage.jsx
+++ khanfused/src/RoomPage.jsx
@@ -24,23 +24,24 @@ function RoomPage() {
 
     return (<> {hasConnected &&
         <div className="roomPage">
-            <div className="logo">
+            <div className="roomPage-logo">
                 <img src={logo} alt="Khanfused Logo" />
             </div>
             <div className="code-section">
-                <p className="instruction-text">Key in the code below:</p>
-                <h1 className="room-code">{code}</h1>
+                <p>Key in the code below:</p>
+                <h1>{code}</h1>
             </div>
             <div className="players-section">
-                <h2>Players:</h2>
+                <h2>Players</h2>
                 <ul className="players-list">
-                    <li className="player"> RICK</li>
-                    <li className="player"> RAY</li>
-                    <li className="player"> TINA</li>
-                    <li className="player"> RENSHYUEN</li>
+                    <li>Ren Shyuen</li>
+                    <li>Rick</li>
+                    <li>Tina</li>
+                    <li>Zhi Yun</li>
+                    <li>Fakerayray</li>
                     <li className="player-input">
                         <input type="text" placeholder="Enter your name" />
-                        <button className="submit-button">Submit</button>
+                        <button>Submit</button>
                     </li>
                 </ul>
             </div>
