f3ba2237cf5b1135cee24e025c227c27c4347cab
Fakerayray <raymondsinglaire@gmail.com>
2024-08-20 23:38:01 +0800

    pip install transitions Added framework for state transitions in lobby.py

--- server/lobby.py
+++ server/lobby.py
@@ -1,11 +1,31 @@
 import random
 import string
+from transitions import Machine
 
 class lobby :
     def __init__(self, password='') :
         self.state = 'waiting'
         self.password = password
         self.players = []
+
+        # Define states and transitions
+        self.states = ['waiting', 'selectingrole', 'spring', 'summer', 'autumn', 'winter', 
+                       'insufficient_food', 'khans_pillaged', 'lords_killed', 'end_game']
+
+        self.transitions = [
+            {'trigger': 'start_role_selection', 'source': 'waiting', 'dest': 'selectingrole'},
+            {'trigger': 'transition_to_season', 'source': 'selectingrole', 'dest': 'spring'},
+            {'trigger': 'transition_to_summer', 'source': 'spring', 'dest': 'summer'},
+            {'trigger': 'transition_to_autumn', 'source': 'summer', 'dest': 'autumn'},
+            {'trigger': 'transition_to_winter', 'source': 'autumn', 'dest': 'winter'},
+            {'trigger': 'transition_back_to_spring', 'source': 'winter', 'dest': 'spring'},
+            {'trigger': 'transition_to_insufficient_food', 'source': 'autumn', 'dest': 'insufficient_food'},
+            {'trigger': 'transition_to_khans_pillaged', 'source': 'autumn', 'dest': 'khans_pillaged'},
+            {'trigger': 'transition_to_lords_killed', 'source': 'winter', 'dest': 'lords_killed'},
+            {'trigger': 'end_game', 'source': '*', 'dest': 'end_game'}
+        ]
+
+        self.machine = Machine(model=self, states=self.states, transitions=self.transitions, initial='waiting')
 
     def start(self) :
         if len(self.players) < 6 :
